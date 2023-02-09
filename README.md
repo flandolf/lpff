e2961444884f875ac166e3434939c328ccd6af3b
andy <66240551+flandolf@users.noreply.github.com>
2023-02-09 19:46:47 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Lichess Puzzle FEN Finder (LPFF)
+This script finds the FEN of a puzzle on lichess.org. It is useful for ~~cheating~~ **solving puzzles**.
+## Dependencies
+- Python 3
+- chess
+- colorama
+- pyperclip
+## Usage
+- Run `python3 export_board.py` in the terminal.
+## Licence
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+## NOTE
+This script is not affiliated with lichess.org in any way. Use at your own risk. If you do decide to use this for cheating, don't blame me if you get banned.
+## FAQ
+- **Q:** Why is the script so slow?
+- **A:** The script is slow because it has to search the database for the FEN.
+- **Q:** Is the code safe?
+- **A:** Look at it yourself!
+## Roadmap
+- GUI Version.
+## Acknowledgements
+- [chess](https://pypi.org/project/chess/)
+- [colorama](https://pypi.org/project/colorama/)
---
+++ export_board.py
@@ -0,0 +1,61 @@
+import chess
+from colorama import Fore
+import time
+import pyperclip
+def is_url(url):
+    if url.startswith("https://lichess.org/training/"):
+        return True
+    else:
+        return False
+while True:
+    url = None
+    if is_url(pyperclip.paste()) == True:
+        url = pyperclip.paste()
+        print(Fore.BLUE + "URL found: " + Fore.RED + url)
+    else:
+        print(Fore.RED + "No URL found in clipboard!" + Fore.RESET)
+        url = input(Fore.CYAN + "Enter URL: " + Fore.RESET)
+        try:
+            if is_url(url) == False:
+                raise ValueError
+        except ValueError:
+            print(Fore.RED + "Invalid URL!" + Fore.RESET)
+            input(Fore.GREEN + "Press enter to continue..." + Fore.RESET)
+            continue
+
+    print(Fore.RED + "Searching for ID..." + Fore.RESET)
+    id = url.split("/")[-1]
+    # Opening the file lichess_db_puzzle.csv and reading it line by line. If the id is in the line, it
+    # splits the line by the comma and takes the second item in the list.
+    start_time = time.time()
+    with open("lichess_db_puzzle.csv", "r") as f:    
+        for line in f:        
+            # print search time
+            if id in line:
+                fen = line.split(",")[1]
+                print(Fore.BLUE + "FEN found: " + Fore.RED + fen)
+                end_time = time.time()
+                search_time = (end_time - start_time) * 1000
+                print(Fore.BLUE + "Search time: " + Fore.RED + str(search_time) + " ms")
+                break
+                
+        f.close()
+
+    board = chess.Board(fen)
+    turn = None
+    if board.turn == True:
+        turn = Fore.WHITE + "White"
+    else:
+        turn = Fore.BLACK + "Black"
+    print(Fore.GREEN + "Turn: " + str(turn))
+    next_move = input(Fore.CYAN + "Enter next move: ")
+    try:
+        board.push_san(next_move)
+    except ValueError:
+        print(Fore.RED + "Invalid move!" + Fore.RESET)
+        input(Fore.GREEN + "Press enter to continue..." + Fore.RESET)
+        continue
+    pyperclip.copy(board.fen())
+    print(Fore.RED + "FEN copied to clipboard!" + Fore.RESET)
+    input(Fore.GREEN + "Press enter to continue..." + Fore.RESET)
+
